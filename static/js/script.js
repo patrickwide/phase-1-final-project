f4ad0d023c32833db100e4832db5d238209e74a9
patrickwide <patrickwide254@gmail.com>
2023-04-08 10:57:19 +0300

    Get references to UI elements and create a function to display stock data in the table

--- static/js/script.js
+++ static/js/script.js
@@ -51,3 +51,192 @@ function createTableRow(data) {
   tr.appendChild(tdAction); // Append the cell with the buttons to the table row
   return tr; // Return the completed table row
 }
+
+// Get references to UI elements
+const buyDateSelect = document.getElementById("buy-date");
+const sellDateSelect = document.getElementById("sell-date");
+const fromDateSelect = document.getElementById("filter-from-date");
+const toDateSelect = document.getElementById("filter-to-date");
+const filterButton = document.getElementById("filter-by-date");
+const tableBody = document.querySelector("tbody");
+const fetchButton = document.getElementById("fetch-symbol");
+const symbolOptions = document.getElementById("fetch-symbol-options");
+const calculateButton = document.getElementById("calculate");
+const numberOfShares = document.getElementById("numberOfShares");
+const buyPrice = document.getElementById("buy-price");
+const sellPrice = document.getElementById("sell-price");
+const results = document.getElementById("results");
+const cursor = document.querySelector(".cursor");
+
+// Function to display stock data in the table
+function displayStockData(data, action = 0) {
+  // Clear the existing rows
+  tableBody.innerHTML = "";
+
+  console.log(action);
+
+  if (action <= 0) {
+    //
+    buyDateSelect.innerHTML = "";
+    sellDateSelect.innerHTML = "";
+    //
+    fromDateSelect.innerHTML = "";
+    toDateSelect.innerHTML = "";
+  }
+
+  let prevBuyButton;
+  let prevSellButton;
+  data.forEach((rowData, index) => {
+    if (action <= 0) {
+      //
+      buyDateSelect.add(new Option(rowData.Date, rowData.Date));
+      sellDateSelect.add(new Option(rowData.Date, rowData.Date));
+      //
+      fromDateSelect.add(new Option(rowData.Date, rowData.Date));
+      toDateSelect.add(new Option(rowData.Date, rowData.Date));
+    }
+
+    // create the data table
+    const tr = createTableRow(rowData);
+    tableBody.appendChild(tr);
+
+    const buyButton = tr.querySelector("button:nth-of-type(1)");
+    buyButton.id = `buy-${index}`;
+    const sellButton = tr.querySelector("button:nth-of-type(2)");
+    sellButton.id = `sell-${index}`;
+
+    buyButton.addEventListener("click", () => {
+      //check if we have a sell button selected and if selected check if date is lesser than the current btn date clicked
+      for (let i = 0; i < buyDateSelect.options.length; i++) {
+        if (buyDateSelect.options[i].value === rowData.Date) {
+          buyDateSelect.selectedIndex = i;
+          break;
+        }
+      }
+      if (prevBuyButton) {
+        prevBuyButton.style = "";
+      }
+      document.getElementById(`buy-${index}`).style.backgroundColor = "#2b2b2b";
+      document.getElementById(`buy-${index}`).style.color = "#f0f0f0";
+      document.getElementById(`buy-${index}`).style.boxShadow =
+        "0 0 5px 2px rgba(0, 255, 0, 0.5)";
+      prevBuyButton = document.getElementById(`buy-${index}`); // Update the previously clicked buy button
+    });
+
+    sellButton.addEventListener("click", () => {
+      //check if we have a buy button selected and if selected check if date is greater than the current btn date clicked
+      if (prevSellButton) {
+        prevSellButton.style = "";
+      }
+      for (let i = 0; i < sellDateSelect.options.length; i++) {
+        if (sellDateSelect.options[i].value === rowData.Date) {
+          sellDateSelect.selectedIndex = i;
+          break;
+        }
+      }
+      document.getElementById(`sell-${index}`).style.backgroundColor =
+        "#2b2b2b";
+      document.getElementById(`sell-${index}`).style.color = "#f0f0f0";
+      document.getElementById(`sell-${index}`).style.boxShadow =
+        "0 0 5px 2px rgba(255, 0, 0, 0.5)";
+      prevSellButton = document.getElementById(`sell-${index}`);
+    });
+  });
+}
+
+const filterDataByDate = (data, startDate, endDate) => {
+  const filteredData = data.filter((item) => {
+    const itemDate = Date.parse(item.Date);
+    const startDateMS = Date.parse(startDate);
+    const endDateMS = Date.parse(endDate);
+    return itemDate >= startDateMS && itemDate <= endDateMS;
+  });
+
+  return filteredData;
+};
+
+// This function takes in an array of stock data, a buy date, a buy price, a sell date,
+// a sell price, and the number of shares bought/sold, and calculates the profit/loss.
+
+function calculateProfit(
+  data,
+  buyDate,
+  buyPrice,
+  sellDate,
+  sellPrice,
+  noShares
+) {
+  // Find the index of the buy and sell dates in the data array.
+  const buyIndex = data.findIndex((d) => d.Date === buyDate);
+  const sellIndex = data.findIndex((d) => d.Date === sellDate);
+
+  // Get the buy and sell prices from the data array.
+  const buyPriceValue = Number(data[buyIndex][buyPrice]);
+  const sellPriceValue = Number(data[sellIndex][sellPrice]);
+
+  // Calculate the total buy and sell prices by multiplying the price by the number of shares.
+  const totalBuyPrice = buyPriceValue * noShares;
+  const totalSellPrice = sellPriceValue * noShares;
+
+  // Calculate the profit/loss by subtracting the total buy price from the total sell price.
+  const profitLoss = totalSellPrice - totalBuyPrice;
+
+  // Return the profit/loss.
+  return [buyPriceValue, sellPriceValue, profitLoss];
+}
+
+calculateButton.addEventListener("click", () => {
+  let response = calculateProfit(
+    data.data,
+    buyDateSelect.value,
+    buyPrice.value,
+    sellDateSelect.value,
+    sellPrice.value,
+    numberOfShares.value
+  );
+
+  results.innerHTML = "";
+
+  const text = `Assuming the purchase date is ${buyDateSelect.value} and the price is $${response[0]}, and the selling date is ${sellDateSelect.value} and the price is $${response[1]}, the profit would be $${response[2]}.`;
+  let i = 0;
+
+  function typeWriter() {
+    if (i < text.length) {
+      results.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 20);
+    } else {
+      cursor.style.display = "none"; // hide cursor when typing is complete
+    }
+  }
+
+  setTimeout(typeWriter, 50);
+
+  // toggle cursor visibility every 500ms
+  setInterval(() => {
+    cursor.style.display =
+      cursor.style.display === "none" ? "inline-block" : "none";
+  }, 500);
+});
+
+filterButton.addEventListener("click", () => {
+  const selectedFromDate = fromDateSelect.value;
+  const selectedToDate = toDateSelect.value;
+  const filteredData = filterDataByDate(
+    data.data,
+    selectedFromDate,
+    selectedToDate
+  );
+  displayStockData(filteredData, 1);
+});
+
+fetchButton.addEventListener("click", async () => {
+  const symbol = symbolOptions.value;
+  data = await fetchStockData(symbol);
+  displayStockData(data.data);
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+  data = await fetchStockData("AAPL");
+  displayStockData(data.data);
+});
